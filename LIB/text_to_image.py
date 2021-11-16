3fa8b5d8ab87599c4a41c2da40d2dbff68cd9f8d
TripleAVerAlpha <TripleAVerAlpha@gmail.com>
2021-11-16 18:54:36 +0300

    Title

--- LIB/text_to_image.py
+++ LIB/text_to_image.py
@@ -10,8 +10,8 @@ def text_to_image(text, file=None):
     test_string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     max_height = pt2px(font.getsize(test_string)[1])
     max_width = pt2px(font.getsize(max_width_line)[0])
-    height = max_height * len(lines)  # perfect or a little oversized
-    width = int(round(max_width + 40))  # a little oversized
+    height = max_height * len(lines)+20  # perfect or a little oversized
+    width = int(round(max_width))+20  # a little oversized
     image = Image.new('RGB', (width, height), color='#0d1117')
     draw_text = ImageDraw.Draw(image)
     draw_text.text((10, 10), text, font=font, fill="#c9d1d9")
@@ -19,3 +19,6 @@ def text_to_image(text, file=None):
     if not (file is None):
         image.save(file)
     return image
+
+
+text_to_image("Мой путь в IT:", "puth.png")
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Мой путь в обучении:<br> 
+![ ](puth.png)<br> 
 **[Decision_Tree_Classifier](Decision_Tree_Classifier)** - *Построение Решающего дерева и трактовка весов*<br>
 **[Pandas](Pandas)** - *Предобработка данных*<br>
 **[Random_Forest_Regressor](Random_Forest_Regressor)** - *Изучение зависимости точности в Случайном лесу*<br>
